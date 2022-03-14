c927ba0d17afb225900f78e29a0763867cf0fec6
christophechr <christophe.chhor@epitech.eu>
2022-03-14 14:57:14 +0100

    edit script to exec without cloning repository

--- install_hymn.sh
+++ install_hymn.sh
@@ -4,10 +4,18 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-#update package
-pacman -Suy
-
 #install sox media player
-pacman -S sox
+dnf -y install sox
+
+#get assets from github
+wget https://github.com/christophechr/ChinaOS/assets/sound.mp3
+
+#copy assets to .china
+cp -r assets/ ~/.china
 
 #include media
+echo alias runsound="sox ~/.china/sound.mp3" >> $HOME/.bashrc
+echo runsound >> $HOME/.bashrc
+
+#reload bash
+exec bash
