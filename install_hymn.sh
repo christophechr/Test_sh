bc04601699eaf0d6f7e6076010df4b14d9dd0d0c
christophechr <christophe.chhor@epitech.eu>
2022-03-18 19:20:20 +0100

    remove sox media player to use default media player in terminal : aplay

--- install_hymn.sh
+++ install_hymn.sh
@@ -1,12 +1,4 @@
 clear
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root" 1>&2
-	exit 1
-fi
-
-#install sox media player
-dnf -y install sox
-
 #get assets from github
 wget https://github.com/christophechr/ChinaOS/assets/sound.mp3
 
@@ -14,7 +6,7 @@ wget https://github.com/christophechr/ChinaOS/assets/sound.mp3
 cp -r assets/ ~/.china
 
 #include media
-echo alias runsound="sox ~/.china/sound.mp3" >> $HOME/.bashrc
+echo alias runsound="aplay ~/.china/sound.mp3" >> $HOME/.bashrc
 echo runsound >> $HOME/.bashrc
 
 #reload bash
