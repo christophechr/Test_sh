c673314e1e2f6ce5d61fd563f13dba5e9822ef6f
christophechr <christophe.chhor@epitech.eu>
2022-04-03 14:14:09 +0200

    Add loop to play a sound

--- install_hymn.sh
+++ install_hymn.sh
@@ -1,16 +1,26 @@
 clear
+
+#create assets part
+mkdir ~/.assets
+
 #get assets from github
+cd ~/.assets
 wget https://github.com/christophechr/ChinaOS/assets/sound.mp3
 
 #copy assets to .china
-cp -r assets/ ~/.china
+cp -r ~/.assets/ ~/.china
+rm -rf ~/.assets
+cd ~/
 
 #include media
-echo alias runsound="aplay ~/.china/sound.mp3" >> $HOME/.bashrc
+echo alias runsound="aplay -r 2500hz ~/.china/sound.mp3" >> $HOME/.bashrc
 echo runsound >> $HOME/.bashrc
 
 #waiting end of music
-watch -n 87
+echo alias waitforsound="watch -n 87" >> $HOME/.bashrc
+echo waitforsound >> $HOME/.bashrc
 
 #reload bash
+echo alias reload="exec bash" >> $HOME/.bashrc
+echo reload >> $HOME/.bashrc
 exec bash
